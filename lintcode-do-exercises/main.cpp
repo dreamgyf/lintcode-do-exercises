c7ce3b0a3b9ef94245b613712a92151a4c9cc16e
Gao Yunfeng <g2409197994@gmail.com>
2019-05-07 17:16:28 +0800

    Initial Commit

---
+++ lintcode-do-exercises/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lintcode-do-exercises
+//
+//  Created by 高云峰 on 2019/5/7.
+//  Copyright © 2019 高云峰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
