61abb28797d9a370f7ec7489c93e664fe24214e3
Gao Yunfeng <g2409197994@gmail.com>
2019-05-09 23:18:48 +0800

    669. 换硬币

--- lintcode-do-exercises/main.cpp
+++ lintcode-do-exercises/main.cpp
@@ -292,6 +292,26 @@ int findTargetSumWays(vector<int> &nums, int s) {
     return findTargetSumWays_res;
 }
 
+//669. 换硬币
+int coinChange(vector<int> &coins, int amount) {
+    // write your code here
+    int dp[amount + 1];
+    dp[0] = 0;
+    for(int i = 1;i <= amount;i++){
+        dp[i] = -1;
+        for(int j = 0;j < coins.size();++j){
+            if(i - coins[j] >= 0 && dp[i - coins[j]] != -1){
+                if(dp[i] == -1)
+                    dp[i] = dp[i - coins[j]] + 1;
+                else {
+                    dp[i] = min(dp[i], dp[i - coins[j]]) + 1;
+                }
+            }
+        }
+    }
+    return dp[amount];
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     vector<int> nums;
