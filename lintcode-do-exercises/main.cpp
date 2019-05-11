0c7713ef09ef6bf6cc7e98ec99aa3f1994113660
Gao Yunfeng <g2409197994@gmail.com>
2019-05-11 14:08:53 +0800

    514. 栅栏染色

--- lintcode-do-exercises/main.cpp
+++ lintcode-do-exercises/main.cpp
@@ -391,6 +391,18 @@ int countNumbersWithUniqueDigits(int n) {
     return sum + 1;
 }
 
+//514. 栅栏染色
+int numWays(int n, int k) {
+    // write your code here
+    int dp[n + 1];
+    dp[1] = k;
+    dp[2] = k * k;
+    for(int i = 3;i <= n;i++){
+        dp[i] = (dp[i - 1] + dp[i - 2]) * (k - 1);
+    }
+    return dp[n];
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     vector<int> nums;
